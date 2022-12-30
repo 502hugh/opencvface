397a6985f7c0155eef08401ba2de63e946317901
lzk30 <121600418+lzk30@users.noreply.github.com>
2022-12-31 01:13:16 +0800

    写了gui的整体布局，以及页面交互的函数 1.1

--- get_face.py
+++ get_face.py
@@ -5,7 +5,7 @@ import tkinter
 from tkinter import font as tkFont
 
 
-import cv2.cv2
+import cv2
 import  dlib
 
 
@@ -28,7 +28,7 @@ class Get_Face:
 
         # 人脸识别的GUI
         self.gui = tkinter.Tk()
-        self.gui.title("人脸识别")
+        self.gui.title("人脸识别 11")
         self.gui.geometry("1400x600")
 
         # 左边的gui部分
