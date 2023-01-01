c4c42cc6bbda9d38d0d5bddee06d59baa7fe86c1
hugh <hugh502@163.com>
2023-01-01 23:28:35 +0800

    补充点文件

---
+++ face_identify.py
@@ -0,0 +1,14 @@
+import dlib
+import numpy as np
+import cv2
+import os
+import pandas as pd
+import time
+import logging
+# Dlib 正向人脸检测器 / Use frontal face detector of Dlib
+detector = dlib.get_frontal_face_detector()
+# Dlib 人脸 landmark 特征点检测器 / Get face landmarks
+predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+
+# Dlib Resnet 人脸识别模型, 提取 128D 的特征矢量 / Use Dlib resnet50 model to get 128D face descriptor
+face_reco_model = dlib.face_recognition_model_v1("data/data_dlib/dlib_face_recognition_resnet_model_v1.dat")
