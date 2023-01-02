89372fd2a4ee6ddc53d2fb85104ca430fc7deb8d
lzk30 <121600418+lzk30@users.noreply.github.com>
2023-01-03 00:35:51 +0800

    Update face_identify.py
    写了函数，从文件中读取出已录入的人脸特征

--- face_identify.py
+++ face_identify.py
@@ -62,3 +62,25 @@ class Face_Recognizer:
         # 如果识别出 "unknown" 的脸, 将在 reclassify_interval_cnt 计数到 reclassify_interval 后, 对于人脸进行重新识别
         self.reclassify_interval_cnt = 0
         self.reclassify_interval = 10 
+        
+        
+            def get_face_database(self):
+        if os.path.exists("data/features_all.csv"):
+            path_features_known_csv = "data/features_all.csv"
+            csv_rd = pd.read_csv(path_features_known_csv, header=None)
+            for i in range(csv_rd.shape[0]):
+                features_someone_arr = []
+                self.face_name_known_list.append(csv_rd.iloc[i][0])
+                for j in range(1, 129):
+                    if csv_rd.iloc[i][j] == '':
+                        features_someone_arr.append('0')
+                    else:
+                        features_someone_arr.append(csv_rd.iloc[i][j])
+                self.face_features_known_list.append(features_someone_arr)
+            logging.info("Faces in Database： %d", len(self.face_features_known_list))
+            return 1
+        else:
+            logging.warning("'features_all.csv' not found!")
+            logging.warning("Please run 'get_faces_from_camera.py' "
+                            "and 'features_extraction_to_csv.py' before 'face_reco_from_camera.py'")
+            return 0
